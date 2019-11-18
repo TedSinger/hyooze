511ea22e99f4640f378db3b22090984d4eedcd3e
Ted Singer <tedsinger1@gmail.com>
2019-11-18 14:42:16 -0500

    better names and comments for perception

--- hyooze/perception.py
+++ hyooze/perception.py
@@ -3,10 +3,12 @@ from colormath.color_conversions import convert_color
 from colormath.color_appearance_models import CIECAM02
 from hyooze import xFF, DEPTH
 
-WHITE_XYZ = convert_color(sRGBColor(1,1,1), XYZColor)
+REFERENCE_WHITE = convert_color(sRGBColor(1,1,1), XYZColor)
 
 
 class Office:
+    """Color perception models are environment dependent. This class encodes the
+    assumptions about viewing conditions."""
     def __init__(self, ambient_lightness, background):
         self.ambient_lightness = ambient_lightness
         self.background = background
@@ -14,14 +16,16 @@ class Office:
         self.background_brightness = background_xyz.xyz_y
         
     def rgb_to_cbh(self, r, g, b):
-        xyz = convert_color(sRGBColor(r, g, b, is_upscaled=True), 
+        target_xyz = convert_color(sRGBColor(r, g, b, is_upscaled=True), 
                             XYZColor)
-        cbh = CIECAM02(
-            xyz.xyz_x, xyz.xyz_y, xyz.xyz_z,
-            WHITE_XYZ.xyz_x, WHITE_XYZ.xyz_y, WHITE_XYZ.xyz_z,
-            c=0.69, n_c=1, f=1,
+        perception = CIECAM02(
+            target_xyz.xyz_x, target_xyz.xyz_y, target_xyz.xyz_z,
+            REFERENCE_WHITE.xyz_x, REFERENCE_WHITE.xyz_y, REFERENCE_WHITE.xyz_z,
+            c=0.69, n_c=1, f=1, # I do not know what these parameters mean
             l_a=self.ambient_lightness, y_b=self.background_brightness)
-        return cbh.colorfulness, cbh.brightness, cbh.hue_angle
+        return perception.colorfulness, perception.brightness, perception.hue_angle
 
-
+# 100 is a typical bright office. I am (optimistically) assuming that:
+# 1) The screen is calibrated to be as bright as the office, and
+# 2) The background is mostly white.
 BRIGHT_OFFICE = Office(100, sRGBColor(1,1,1))
