fd869cfe9b0fac415db27aa6da324f84ecef94bc
Ted Singer <tedsinger1@gmail.com>
2020-06-11 22:52:03 -0400

    less silly structures

--- hyooze/main.py
+++ hyooze/main.py
@@ -9,23 +9,25 @@ conn = get_conn()
 greys =  ['#202020'] + fit_greys_between(7, '#202020', '#f3f3f3', conn) + ['#f3f3f3'] 
 
 # FIXME: question mark?
-MIN_CHROMA_TIMES_HUE_DIFF = 5000000 * 360 / 8
+MIN_CHROMA_TIMES_HUE_DIFF = 3000000 * 360 / 8
 
-# FIXME: wrong data structure
+
 color_scheme = []
 
 for grey in greys:
+    colors_for_level = [[grey]]
     print(grey)
     brightness = grey_to_brightness(grey, conn)
     chroma_extremes = chroma_extremes_for_brightness(brightness, conn)
     ring_size = chroma_ring_size(chroma_extremes)
-    color_scheme.append([grey])
+    
     for chroma_target in range(int(ring_size), max(chroma_extremes) + 1, int(ring_size)):
         print(chroma_target)
         ring_colors = chroma_ring(brightness, chroma_target, conn)
-        hues = [h for _, h, _ in ring_colors]
-        chosen_hues = choose_hues(hues, MIN_CHROMA_TIMES_HUE_DIFF / (chroma_target + 1))
+        hexcodes_by_hue = dict((h, hexcode) for _, h, hexcode in ring_colors)
+        chosen_hues = choose_hues(list(hexcodes_by_hue.keys()), MIN_CHROMA_TIMES_HUE_DIFF / (chroma_target + 1))
         print(chosen_hues)
-        # FIXME: this is not a hashmap
-        chosen_colors = [hexcode for _, h, hexcode in ring_colors if h in chosen_hues]
-        color_scheme.append(chosen_colors)
+        
+        chosen_colors = [hexcodes_by_hue[h] for h in chosen_hues]
+        colors_for_level.append(chosen_colors)
+    color_scheme.append(colors_for_level)
