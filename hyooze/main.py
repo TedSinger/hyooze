115830e554f7bb891dd6f37780b09df8da3330eb
Ted Singer <tedsinger1@gmail.com>
2020-09-11 00:21:05 -0400

    svgs loaded as resources

--- hyooze/main.py
+++ hyooze/main.py
@@ -12,12 +12,11 @@ import pandas
 conn = get_conn()
 
 greys = fit_greys_between(9, '#000000', '#f7f7f6', conn) + ['#f7f7f6']
-
+brightnesses = [grey_to_brightness(grey, conn) for grey in greys]
 colors = []
 
-for grey in greys:
+for grey, brightness in zip(greys, brightnesses):
     print(grey)
-    brightness = grey_to_brightness(grey, conn)
 
     area = chroma_area(max_chroma_boundary(brightness, conn))
     these_colors = select_colors(brightness, 4 + int(area / 4.5e8), conn)
@@ -26,41 +25,58 @@ for grey in greys:
 
 df = pandas.DataFrame(data=colors, columns=["brightness", "chroma", "hue","hexcode"])
 
+h0, h1 = (-180, 180)
+h_size = h1 - h0
+r = 10
+height = 300
+width = 400
 
 def svg_elems(colors):
-    # force to correct number of columns
-    h0, h1 = (-180, 180)
-    h_size = h1 - h0
-    r = 20
-    height = 750
-    width = 1500
-    # FIXME: extend by half a column in both directions
-    b0, b1 = (colors['brightness'].min(), colors['brightness'].max())
-    b_size = b1 - b0
     def scale_brightness(b):
-        return round((width - 2*r) * ((b - b0) / b_size), 2) + r
+        index = min(range(len(greys)), key=lambda i: abs(brightnesses[i]-b))
+        return round((width - 4*r) * index/(len(greys) - 1), 2) + 2*r
     def scale_hue(h):
-        return round((height - 2*r) * ((h - h0) / h_size), 2) + r
+        return round((height - 4*r) * ((h - h0) / h_size), 2) + 2*r
     def row_to_circle(row):
         (b,_,h,hexcode) = row
         return f'<circle cx="{scale_brightness(b)}" cy="{scale_hue(h)}" r="{r}" fill="{hexcode}"><title>{hexcode}</title></circle>'
     return colors.apply(row_to_circle, axis=1)
 
+def grey_circles(greys):
+    ret = ''
+    for i, hexcode in enumerate(greys):
+        ret += f'<circle cx="{round((width - 4*r) * i/(len(greys) - 1), 2) + 2*r}" cy="{2*r}" r="{r}" fill="{hexcode}"><title>{hexcode}</title></circle>'
+    return ret
 
-print('<html><body>')
-
-chroma_quantiles = [0, .511, 1]
+text = '<html><body>'
+svg = f'''<svg xmlns="http://www.w3.org/2000/svg" width="{width}" height="{4*r}">
+        <rect width="50%" height="100%" fill="white"></rect>
+        <rect x="50%" width="50%" height="100%" fill="black"></rect>
+        <rect width="100%" height="100%" fill="none" stroke="#888888" stroke-width="1px"></rect>
+        {grey_circles(greys)}
+        </svg>'''
+text += svg
+with open('grey-palette.svg', 'w') as f:
+    f.write(svg)
+chroma_quantiles = [0, .333, .667, 1]
+names = ['subtle-palette.svg', 'muted-palette.svg', 'loud-palette.svg']
 for c_idx in range(len(chroma_quantiles) - 1):
     c_min = df['chroma'].quantile(chroma_quantiles[c_idx])
     c_max = df['chroma'].quantile(chroma_quantiles[c_idx + 1])
     colors_matching_both = df[(df['chroma'] >= c_min) & (df['chroma'] <= c_max)]
     s = '\n'.join(svg_elems(colors_matching_both).tolist())
-    svg = f'''<svg width="1500" height="750">
+    svg = f'''<svg xmlns="http://www.w3.org/2000/svg" width="{width}" height="{height}">
         <rect width="50%" height="100%" fill="white"></rect>
         <rect x="50%" width="50%" height="100%" fill="black"></rect>
+        <rect width="100%" height="100%" fill="none" stroke="#888888" stroke-width="1px"></rect>
         {s}
         </svg>'''
-    print(svg)
-    print()
-print('</body></html>')
+    with open(names[c_idx], 'w') as f:
+        f.write(svg)
+    text += '<br> <br>'
+    text += svg
+
+text += '</body></html>'
 # FIXME: show greys, too
+with open('/dev/shm/foo.html', 'w') as f:
+    f.write(text)
